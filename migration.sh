11d3f26ae9096e7ea342d7d8a7cdd82caa634531
Laura Gerhardt <laura.gerhardt@gsa.gov>
2017-07-31 14:58:11 -0400

    finish middlelayer script

--- migration.sh
+++ migration.sh
@@ -14,8 +14,8 @@ cf login -sso
 cf t -o $ORGNAME
 
 # Create spaces
-cf create-space middlelayer-api-staging
-cf create-space middlelayer-api-production
+cf create-space api-staging
+cf create-space api-production
 cf create-space public-staging
 cf create-space public-production
 
@@ -48,34 +48,44 @@ cf delete-route -f app.cloud.gov --hostname $3
 freeOldOrgUrl fs-api-staging fs-middlelayer-api-staging fs-middlelayer-api-staging
 
 # Update cg-deploy orgs to Org name
+findAndReplace()
+{
+  for f in $1
+  do
+    if [ -f $f -a -r $f ]; then
+      sed -i "s/$2/$3/g" "$f"
+     else
+      echo "Error: Cannot read $f"
+    fi
+  Done
+}
+
 updateDeployementOrgs()
 {
-
-git checkout $1
-DPATH="cg-deploy/*"
-for f in $DPATH
-do
-  if [ -f $f -a -r $f ]; then
-    /bin/cp -f $f $BPATH
-    sed -i "s/$OLDORG/$ORGNAME/g" "$f"
-   else
-    echo "Error: Cannot read $f"
-  fi
-Done
-
-git add .
-git commit -m “update deployment to $ORGNAME”
-git push origin $1
+  git checkout $1
+  findAndReplace $3 $4 $5
+  git add .
+  git commit -m $2
+  git push origin $1
 }
-updateDeployementOrgs dev
+
+deployerChanges()
+{
+  updateDeployementOrgs $1 “update deployment to $ORGNAME” "cg-deploy/*" $OLDORG $ORGNAME
+  updateDeployementOrgs $1 “update prod space name” "*" $2 $3
+  updateDeployementOrgs $1 “update dev space name” "*" $4 $5
+}
+deployerChanges dev fs-api-prod api-production fs-api-staging api-staging
+deployerChanges master fs-api-prod api-production fs-api-staging api-staging
+
 
 # Push app on new org
-cf t -o $ORGNAME -s middlelayer-api-production
+cf t -o $ORGNAME -s api-production
 git checkout master # not sure if this makes sense
 cf push fs-middlelayer-api -f "./cg-deploy/manifests/manifest.yml"
 
-cf t -s middlelayer-api-staging
-cf push middlelayer-api-staging -f "./cg-deploy/manifests/manifest.yml"
+cf t -s api-staging
+cf push middlelayer-api-staging -f "./cg-deploy/manifests/manifest-staging.yml"
 
 # Intake
 cd ..
@@ -87,7 +97,7 @@ cf create-service aws-rds shared-psql intake-db
 cf create-service s3 basic intake-s3
 cf create-service cloud-gov-service-account space-deployer intake-deployer
 cf service-key my-service-account intake-deployer
-cf cups -p middlelayer-service $2 ADD VALUES
+cf cups -p middlelayer-service -p
 cf cups -p intake-auth-service $3 ADD VALUES
 }
 
