68ac28b76ff42e07e5be4de4ba5956972d573159
Laura Gerhardt <laura.gerhardt@gsa.gov>
2017-08-04 11:46:09 -0400

    update login service

--- bin/intake.sh
+++ bin/intake.sh
@@ -22,8 +22,8 @@ createIntakeServices()
   cf cups intake-client-service -p "${INTAKE_AUTH_SERVICE_JSON}"
 
   #Todo eAuth and login services
-  INTAKE_AUTH_SERVICE_JSON="{\"cert\": \"${8}\", \"entrypoint\": \"https://idp.int.login.gov/api/saml/auth\", \"issuer\": \"${9}\", \"privatekey\": \"a\"}"
-  cf cups login-service-provider -p "${INTAKE_AUTH_SERVICE_JSON}"
+  INTAKE_LOGIN_SERVICE_JSON="{\"cert\": \"${8}\", \"entrypoint\": \"https://idp.int.login.gov/api/saml/auth\", \"issuer\": \"${9}\", \"privatekey\": \"${10}\"}"
+  cf cups login-service-provider -p "${INTAKE_LOGIN_SERVICE_JSON}"
 }
 
 freeOldIntakeOrgUrls()
--- bin/util.sh
+++ bin/util.sh
@@ -21,14 +21,14 @@ updateDeployementOrgs()
 
 deleteService()
 {
-  cf unbind-service "${1}" "${2}"
-  cf delete-service "${2}" -f
+  cf t -s "${1}"
+  cf unbind-service "${2}" "${3}"
+  cf delete-service "${3}" -f
 }
 
 rebuildCupsService() #space #bound-app #service #json
 {
-  cf t -s "${1}"
-  delete-service "${2}" "${3}"
+  delete-service "${1}" "${2}" "${3}"
   cf cups "${3}" -p "${4}"
   cf bind-service "${2}" "${3}"
   cf restage "${2}"
